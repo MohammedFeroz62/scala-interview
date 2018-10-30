9b480bee1212763f0a6f03abd8455d5a2cca48a2
MohammedFeroz62 <42738580+MohammedFeroz62@users.noreply.github.com>
2018-10-30 20:13:47 +0100

    Update WhatsWrong1.scala

--- app/WhatsWrong1.scala
+++ app/WhatsWrong1.scala
@@ -12,6 +12,12 @@ object WhatsWrong1 {
     override val city = "Paris"
   }
 
-  Supporter.city //What does this print ?
-  Supporter.support //What does this print and why ? How to fix it ?
-}
+  Supporter.city //What does this print ?   
+  Supporter.support //What does this print and why ? How to fix it ? 
+}  
+
+
+Answer : Paris,  because it is overrided
+Answer : It does not print any value because the string shouble declared in the double quotes.
+         2) If s is declared in the double qoutes then the ouput will be : s Ici c'est $city !
+         3) If s is deleted then the ouput will be : Ici c'est $city !
