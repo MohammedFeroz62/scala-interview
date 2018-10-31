1bfd24f8fe73ef1c01f5b3990bd9f1b1cfc4816c
MohammedFeroz62 <42738580+MohammedFeroz62@users.noreply.github.com>
2018-10-31 08:42:31 +0100

    Update WhatsWrong3.scala

--- app/WhatsWrong3.scala
+++ app/WhatsWrong3.scala
@@ -7,8 +7,8 @@ import scala.util.{ Failure, Success }
 import scala.concurrent.ExecutionContext.Implicits.global
 
 /*
-Do you see anything that could lead to potential problems ?
-What would you do to fix it ?
+Do you see anything that could lead to potential problems ?-------- Answer :ExecutionContext.global is an ExecutionContext backed by a ForkJoinPool
+What would you do to fix it ?                                               A general purpose ExecutionContext must be asynchronous in executing any Runnable that is passed into its execute-method. A special purpose ExecutionContext may be synchronous but must only be passed to code that is explicitly safe to be run using a synchronously executing ExecutionContext.
 Do not mind about the not implemented code
 */
 
@@ -26,7 +26,7 @@ class WhatsWrong3 extends Actor {
     }
   }
 
-  def handleResponse(r: String) = ??? // mutate internal state
+  def handleResponse(r: String) = ??? // mutate internal state---------Answer: query("queryAsyncServer")
 
-  def queryAsyncServer(): Future[String] = ???
+  def queryAsyncServer(): Future[String] = ???----------- Receive("handleResponse")
 }
